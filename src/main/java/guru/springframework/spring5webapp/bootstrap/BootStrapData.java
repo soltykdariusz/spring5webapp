c44902ce8267525624eeae0b20d9fba7b60ea1b2
dsoltyk <dariusz.soltyk@gmail.com>
2020-02-03 20:35:24 +0100

    Added Publisher

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -27,24 +27,32 @@ public class BootStrapData implements CommandLineRunner {
 
         Author eric = new Author("Eric", "Newman");
         Book domain = new Book("Domain Driven Design", "666");
-        Publisher dataPublisher = new Publisher("Hill", "London", "State", "1234" );
+        Publisher publisher = new Publisher("Adress","London","State","12345");
         eric.getBooks().add(domain);
         domain.getAuthors().add(eric);
 
+        domain.setPublisher(publisher);
+        publisher.getBooks().add(domain);
+
         authorRepository.save(eric);
         bookRepository.save(domain);
-        publisherRepository.save(dataPublisher);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Jonson");
         Book noEJB = new Book("J2EE Development without EJB", "666666");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
         System.out.println(publisherRepository.count());
         System.out.println("Number of books: " + bookRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
 
     }
 }
