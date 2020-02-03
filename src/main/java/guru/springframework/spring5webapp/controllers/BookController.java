c223bea33220716fb004c3392da2d500c0619f8a
dsoltyk <dariusz.soltyk@gmail.com>
2020-02-04 00:15:48 +0100

    Add response

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,8 +1,11 @@
 package guru.springframework.spring5webapp.controllers;
 
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,11 +17,16 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/books")
+    /*@RequestMapping("/books")
     public String getBooks(Model model){
 
         model.addAttribute("books", bookRepository.findAll());
 
         return "books";
+    }*/
+
+    @GetMapping("/book")
+    public ResponseEntity<String> _() {
+        return new ResponseEntity<>("{\"status\" : \"OK\"}", HttpStatus.OK);
     }
 }
